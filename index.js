8c2b9ee653c7e06b107212ddb08038c678120b37
FrankZZ <frank@wamm.es>
2016-07-14 16:32:57 +0200

    Improve messages on Slack

--- index.js
+++ index.js
@@ -3,21 +3,22 @@
 var lib = require('./lib');
 lib.slack('Taking an *AwesomeSelfie* in 3 seconds!', function () {
     setTimeout(function () {
-        lib.camera.start(function (err) {
-            if (err) {
-                console.log('ERROR', err);
-                return;
-            }
-            lib.fileUpload.upload('/tmp/selfie.gif', function (err, data) {
+        lib.slack(':D Cheese! :D', function () {
+            lib.camera.start(function (err) {
                 if (err) {
                     console.log('ERROR', err);
                     return;
                 }
-                lib.slack('Oh my god, it\'s awesome! \nhttp://awesomeselfiemachine.s3-website-eu-west-1.amazonaws.com/' + data.key, function () {
-                    console.log('DONE');
+                lib.fileUpload.upload('/tmp/selfie.gif', function (err, data) {
+                    if (err) {
+                        console.log('ERROR', err);
+                        return;
+                    }
+                    lib.slack('Oh my god, it\'s *Awesome*! \nhttp://awesomeselfiemachine.s3-website-eu-west-1.amazonaws.com/' + data.key, function () {
+                        console.log('DONE');
+                    });
                 });
             });
-
         });
     }, 3000);
 });
