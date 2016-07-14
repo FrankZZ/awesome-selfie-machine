44e7d042c0b70b4b8214ef46df9ea1e519abf9a2
FrankZZ <frank@wamm.es>
2016-07-14 16:29:50 +0200

    Add progress message

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 'use strict';
 
 var lib = require('./lib');
-lib.slack('Taking an *AwesomeSelfie*! Just hold on...', function () {
-    lib.camera.start(function (err) {
-        lib.fileUpload.upload('/tmp/selfie.gif', function (err, data) {
+lib.slack('Taking an *AwesomeSelfie* in 3 seconds!', function () {
+    setTimeout(function () {
+        lib.camera.start(function (err) {
             if (err) {
                 console.log('ERROR', err);
                 return;
             }
-            lib.slack('http://awesomeselfiemachine.s3-website-eu-west-1.amazonaws.com/' + data.key, function () {
-                console.log('DONE');
+            lib.fileUpload.upload('/tmp/selfie.gif', function (err, data) {
+                if (err) {
+                    console.log('ERROR', err);
+                    return;
+                }
+                lib.slack('http://awesomeselfiemachine.s3-website-eu-west-1.amazonaws.com/' + data.key, function () {
+                    console.log('DONE');
+                });
             });
-        });
 
-    });
+        });
+    }, 3000);
 });
