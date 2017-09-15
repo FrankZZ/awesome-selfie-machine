51dc37591ba46e232fa2d0ffe3b86a3716c92c84
FrankZZ <frank@wamm.es>
2017-09-15 22:32:07 +0200

    Announcing picture,success,failure with milight

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 'use strict';
 
 var lib = require('./lib');
-lib.slack('Taking an *AwesomeSelfie* in 3 seconds!', function () {
-    setTimeout(function () {
-        lib.slack(':D Cheese! :D', function () {
-            lib.camera.start(function (err) {
+lib.lights.announcePicture()
+    .then(function () {
+        lib.camera.start(function (err) {
+            if (err) {
+                console.log('ERROR', err);
+                lib.lights.announceFailure();
+                return;
+            }
+            lib.fileUpload.upload('/tmp/selfie.gif', function (err, data) {
                 if (err) {
+                    lib.lights.announceFailure();
                     console.log('ERROR', err);
                     return;
                 }
-                lib.fileUpload.upload('/tmp/selfie.gif', function (err, data) {
-                    if (err) {
-                        console.log('ERROR', err);
-                        return;
-                    }
-                    lib.slack('Oh my god, it\'s *Awesome*! \nhttp://awesomeselfiemachine.s3-website-eu-west-1.amazonaws.com/' + data.key, function () {
-                        console.log('DONE');
+
+                lib.lights.announceSuccess()
+                    .then(function () {
+                        console.log('DONE');                            
                     });
-                });
             });
         });
-    }, 3000);
-});
+    });
